e75da2bfbab6786142aa0dd579d0bbcf13d87eab
Jenny Ragan <ruwebtech@gmail.com>
2019-02-07 19:48:48 -0500

    add js to intercept elementor navigation

--- facetwp-elementor.php
+++ facetwp-elementor.php
@@ -166,6 +166,25 @@ class FacetWP_El_Integration {
                             elementorFrontend.init();
                         }
                     });
+                    $(document).on('click', '.facetwp-template .elementor-pagination a', function(e) {
+                        e.preventDefault();
+                        var matches = $(this).attr('href').match(/\/page\/(\d+)/);
+                        if (null !== matches) {
+                            FWP.paged = parseInt(matches[1]);
+                            FWP.soft_refresh = true;
+                            FWP.refresh();
+                        }
+                    });
+                    $(document).on('click', '.facetwp-template .woocommerce-pagination a', function(e) {
+                        e.preventDefault();
+                        var matches = $(this).attr('href').match(/product-page=(\d+);
+                        console.log( matches )
+                        if (null !== matches) {
+                            FWP.paged = parseInt(matches[1]);
+                            FWP.soft_refresh = true;
+                            FWP.refresh();
+                        }
+                    });
                 })(jQuery);
             </script>
         <?php endif;
