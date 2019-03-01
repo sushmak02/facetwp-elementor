9cb0f5290b7641c218bafa2a24c756bd5f086953
Jenny Ragan <ruwebtech@gmail.com>
2019-03-01 14:20:15 -0500

    added pagination for posts module

--- assets/js/front.js
+++ assets/js/front.js
@@ -4,9 +4,18 @@
             elementorFrontend.init();
         }
     });
-    $(document).on('click', '.facetwp-template .elementor-pagination a', function(e) {
+    $(document).on('click', '.facetwp-template.elementor-widget-archive-posts .elementor-pagination a', function(e) {
         e.preventDefault();
         var matches = $(this).attr('href').match(/\/page\/(\d+)/);
+        if (null !== matches) {
+            FWP.paged = parseInt(matches[1]);
+            FWP.soft_refresh = true;
+            FWP.refresh();
+        }
+    });
+    $(document).on('click', '.facetwp-template.elementor-widget-posts .elementor-pagination a', function(e) {
+        e.preventDefault();
+        var matches = $(this).attr('href').match(/\/(\d+)/);
         if (null !== matches) {
             FWP.paged = parseInt(matches[1]);
             FWP.soft_refresh = true;
